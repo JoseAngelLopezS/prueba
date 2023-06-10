354e2b047ff2174a7387671ae4d8a62b3a149a12
JoseAngelLopezS <135417182+JoseAngelLopezS@users.noreply.github.com>
2023-06-09 21:48:16 -0600

    Add files via upload

---
+++ objetos.js
@@ -0,0 +1,11 @@
+const objeto = {
+    propiedad1: 'valor1',
+    propiedad2: 'valor2',
+  };
+  
+  const propiedad = 'propiedad1';
+  
+  console.log(objeto[propiedad]); 
+  
+  console.log(objeto[propiedad]); 
+  
